fbef0e1c81517e8b391f6db92b1337483ff967d1
vietdt <vietdt@gmail.com>
2013-10-05 03:37:45 +0700

    class size and budget requested changes and enable campers to see the "comments for board only" entry box

--- docent/workshops/vocabularies.py
+++ docent/workshops/vocabularies.py
@@ -30,3 +30,31 @@ class ClassLengthVocab(object):
                                  SimpleTerm(value=225, title=_(u'225')),
                                  SimpleTerm(value=300, title=_(u'300'))])
 grok.global_utility(ClassLengthVocab, name=u"docent.workshops.vocab.class_length")
+
+class ClassSizeLimitVocab(object):
+    """
+    Return a vocab of numbers for Class Size Limit
+    """
+    grok.implements(IVocabularyFactory)
+    
+    def __call__(self, context):
+        return SimpleVocabulary([SimpleTerm(value=None, title=_(u'-- Select --')),
+                                 SimpleTerm(value=10, title=_(u'10')),
+                                 SimpleTerm(value=15, title=_(u'15')),
+                                 SimpleTerm(value=20, title=_(u'20')),
+                                 SimpleTerm(value=25, title=_(u'25'))])
+grok.global_utility(ClassSizeLimitVocab, name=u"docent.workshops.vocab.class_size_limit")
+
+class BudgetRequestedVocab(object):
+    """
+    Return a vocab of numbers for Budget Requested
+    """
+    grok.implements(IVocabularyFactory)
+    
+    def __call__(self, context):
+        return SimpleVocabulary([SimpleTerm(value=5, title=_(u'5')),
+                                 SimpleTerm(value=10, title=_(u'10')),
+                                 SimpleTerm(value=15, title=_(u'15')),
+                                 SimpleTerm(value=25, title=_(u'25')),
+                                 SimpleTerm(value=20, title=_(u'50'))])
+grok.global_utility(BudgetRequestedVocab, name=u"docent.workshops.vocab.budget_requested")
